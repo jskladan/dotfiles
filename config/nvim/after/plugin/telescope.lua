65a94b7d8d96fe08e23cb6381494d9169b772104
Josef Skladanka <josef.skladanka@gmail.com>
2023-10-20 14:54:18 +0200

    Nvim - Telescope - added fzf syntax reference

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -18,6 +18,17 @@ require('telescope').setup {
     }
 }
 
+-- FZF Syntax
+-- | Token     | Match type                 | Description                          |
+-- | --------- | -------------------------- | ------------------------------------ |
+-- | `sbtrkt`  | fuzzy-match                | Items that match `sbtrkt`            |
+-- | `'wild`   | exact-match (quoted)       | Items that include `wild`            |
+-- | `^music`  | prefix-exact-match         | Items that start with `music`        |
+-- | `.mp3$`   | suffix-exact-match         | Items that end with `.mp3`           |
+-- | `!fire`   | inverse-exact-match        | Items that do not include `fire`     |
+-- | `!^music` | inverse-prefix-exact-match | Items that do not start with `music` |
+-- | `!.mp3$`  | inverse-suffix-exact-match | Items that do not end with `.mp3`    |
+
 require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
